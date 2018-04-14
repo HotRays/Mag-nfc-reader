e53556bec549c75b8ae27e2e86f05900f45e630f
rjp0817 <18434366461@163.com>
2018-04-14 16:50:12 +0800

    add reset

--- examples/ble_peripheral/ble_app_my_mrfc630_right/main.c
+++ examples/ble_peripheral/ble_app_my_mrfc630_right/main.c
@@ -1,102 +1,4 @@
 
-//#include <stdint.h>
-//#include <string.h>
-//#include "nrf.h"
-//#include "nrf_drv_twi.h"
-//#include "app_uart.h"
-//#include "mfrc630.h"
-//#include "nrf_delay.h"
-
-//#define UART_TX_BUF_SIZE      512                   /**< UART TX buffer size. */
-//#define UART_RX_BUF_SIZE      512                   /**< UART RX buffer size. */
-
-//static ble_nus_t              m_nus;                    /**< Structure to identify the Nordic UART Service. */
-//static uint16_t               m_ble_nus_max_data_len = BLE_GATT_ATT_MTU_DEFAULT - 3;  /**< Maximum length of data (in bytes) that can be transmitted to the peer by the Nordic UART service module. */
-
-//void uart_event_handle(app_uart_evt_t * p_event)
-//{
-//    static uint8_t data_array[BLE_NUS_MAX_DATA_LEN];
-//    static uint8_t index = 0;
-//    uint32_t       err_code;
-
-//    switch (p_event->evt_type)
-//    {
-//        case APP_UART_DATA_READY:
-//            UNUSED_VARIABLE(app_uart_get(&data_array[index]));
-//            index++;
-
-//            if ((data_array[index - 1] == '\n') || (index >= (m_ble_nus_max_data_len)))
-//            {
-//                NRF_LOG_DEBUG("Ready to send data over BLE NUS\r\r\n");
-//                NRF_LOG_HEXDUMP_DEBUG(data_array, index);
-
-//                do
-//                {
-//                    err_code = ble_nus_string_send(&m_nus, data_array, index);
-//                    if ( (err_code != NRF_ERROR_INVALID_STATE) && (err_code != NRF_ERROR_BUSY) )
-//                    {
-//                        APP_ERROR_CHECK(err_code);
-//                    }
-//                } while (err_code == NRF_ERROR_BUSY);
-
-//                index = 0;
-//            }
-//            break;
-
-//        case APP_UART_COMMUNICATION_ERROR:
-//            APP_ERROR_HANDLER(p_event->data.error_communication);
-//            break;
-
-//        case APP_UART_FIFO_ERROR:
-//            APP_ERROR_HANDLER(p_event->data.error_code);
-//            break;
-
-//        default:
-//            break;
-//    }
-//}
-
-//static void uart_init(void)
-//{
-//    uint32_t                     err_code;
-//    const app_uart_comm_params_t comm_params =
-//    {
-//        .rx_pin_no    = RX_PIN_NUMBER,
-//        .tx_pin_no    = TX_PIN_NUMBER,
-//        .rts_pin_no   = RTS_PIN_NUMBER,
-//        .cts_pin_no   = CTS_PIN_NUMBER,
-//        .flow_control = APP_UART_FLOW_CONTROL_DISABLED,
-//        .use_parity   = false,
-//        .baud_rate    = UART_BAUDRATE_BAUDRATE_Baud115200
-//    };
-
-//    APP_UART_FIFO_INIT(&comm_params,
-//                       UART_RX_BUF_SIZE,
-//                       UART_TX_BUF_SIZE,
-//                       uart_event_handle,
-//                       APP_IRQ_PRIORITY_LOWEST,
-//                       err_code);
-//    APP_ERROR_CHECK(err_code);
-//}
-
-//int main(void)
-//{
-//	uart_init();
-//	mfrc630_twi_init();
-//    mfrc630_AN1102_recommended_registers(MFRC630_PROTO_ISO14443A_106_MILLER_MANCHESTER);
-//    uint8_t i=0;
-//	for (;;)
-//    {   
-//		mfrc630_MF_example_dump();
-//		nrf_delay_ms(1000);
-//		printf("count = %d\r\n\r", i++);
-//		printf("\r\n\r");
-//			
-//    }
-//	return 0;
-//}
-
-
 #include <stdint.h>
 #include <string.h>
 #include "nordic_common.h"
@@ -215,7 +117,7 @@ static void nus_data_handler(ble_nus_t * p_nus, uint8_t * p_data, uint16_t lengt
 {
     uint32_t err_code;
 
-    NRF_LOG_DEBUG("Received data from BLE NUS. Writing data on UART.\r\n");
+  NRF_LOG_DEBUG("Received data from BLE NUS. Writing data on UART.\r\n");
 	ble_nus_string_send(&m_nus, p_data, length);
 	for(uint32_t i = 0; i < length; i++)
 	{
@@ -815,26 +717,34 @@ int main(void)
 	  mfrc630_twi_init();
     mfrc630_AN1102_recommended_registers(MFRC630_PROTO_ISO14443A_106_MILLER_MANCHESTER);
 	
+	    bsp_board_leds_init();
+
     err_code = ble_advertising_start(BLE_ADV_MODE_FAST);
     APP_ERROR_CHECK(err_code);
 
-	uint8_t i;
+	uint8_t ii = 0;
 	for (;;)
 	{   
-	mfrc630_MF_example_dump();
-	nrf_delay_ms(1000);
-	printf("count = %d\r\n\r", i++);
-	printf("\r\n\r");			
+		mfrc630_MF_example_dump();
+		nrf_delay_ms(1000);
+		printf("count = %d\r\n\r", ii++);			
 	}
 }
 
 
-//void main()
-//{
-//	uart_init();
-//	uint8_t a;
-//	a = 255;
-//	printf("a = %d\r\n", a);
-//	nrf_delay_ms(1000);
 
-//}
+
+//复位 重启
+//static void (*reset_this_CPU)(void) = 0x0000; 
+//reset_this_CPU(); //***跳到0x0000地址指针，也就是复位
+
+
+//	bsp_board_leds_init();
+//	nrf_gpio_pin_write(BSP_LED_0,0);
+//	nrf_gpio_pin_write(BSP_LED_1,1);
+
+//	nrf_delay_ms(1);
+
+//	nrf_gpio_pin_write(BSP_LED_0,1);
+//	nrf_gpio_pin_write(BSP_LED_1,0);
+//	nrf_delay_ms(1);
